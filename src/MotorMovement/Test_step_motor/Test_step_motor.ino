02f9c06d8aec845460bcd14aed86bfe806183710
Osmar <osmar.ff.1996@gmail.com>
2019-10-31 13:30:11 +0100

    Add y motor in the code

--- src/MotorMovement/Test_step_motor/Test_step_motor.ino
+++ src/MotorMovement/Test_step_motor/Test_step_motor.ino
@@ -11,6 +11,7 @@
 #define MIN_Y_PIN 8
 #define MAX_X_PIN 6
 #define MAX_Y_PIN 5
+#define MAGNET_PIN 0
 
 //CONFIG
 #define STATES_DIF 40
@@ -21,12 +22,15 @@
 int velocidade_motor = 1000;
 int aceleracao_motor = 100;
 int max_x_step = 0;
+int max_y_step = 0;
 long x_array[19] = {0};
 long y_array [17] = {0};
 String cmd_input;
+int chosen_position = 0;
 
 
 AccelStepper motorX(1, 7, 4 );
+AccelStepper motorY(2, 8, 5 );
 
 void setup()
 {
@@ -34,12 +38,16 @@ void setup()
   pinMode(EN, OUTPUT);
   pinMode(MIN_X_PIN, INPUT);
   pinMode(MAX_X_PIN, INPUT);
+  pinMode(MAGNET_PIN, OUTPUT);
+  digitalWrite(MAGNET_PIN, LOW);
   motorX.setMaxSpeed(MOTOR_SPEED);
   motorX.setSpeed(MOTOR_SPEED);
   motorX.setAcceleration(MOTOR_ACC);
+  motorY.setMaxSpeed(MOTOR_SPEED);
+  motorY.setSpeed(MOTOR_SPEED);
+  motorY.setAcceleration(MOTOR_ACC);
   calibration();
-  Serial.println("Type \"AT+GOTO(a number between 1 and 19)\" for the x motion");
-  Serial.println("Type \"AT+GOTO(a number between 1 and 17)\" for the y motion");
+  Serial.println("Type \"AT+GOTO(a number between 1 and 19)(X or Y)\"");
   
 }
 
@@ -50,65 +58,30 @@ void loop()
     cmd_input = Serial.readString();
     if (cmd_input.startsWith("AT+GOTO")){
       cmd_input.remove (0,7);
-      if (cmd_input.toInt()>0 and cmd_input.toInt()<20){
-         Serial.print("Going to position: ");
-         Serial.println(cmd_input.toInt());
-         motorX.moveTo(x_array[cmd_input.toInt()-1]);
-         digitalWrite(EN, LOW);
+      if (cmd_input.endsWith("X")){
+        if (cmd_input.toInt()>0 and cmd_input.toInt()<20){
+           Serial.print("Going to position X: ");
+           Serial.println(cmd_input.toInt());
+           motorX.moveTo(x_array[cmd_input.toInt()-1]);
+           digitalWrite(EN, LOW);
+        }
+        Serial.println ("Invalid position");
       }
-      Serial.println ("Invalid position");
+      if (cmd_input.endsWith("Y")){
+        if (cmd_input.toInt()>0 and cmd_input.toInt()<18){
+           Serial.print("Going to position Y: ");
+           Serial.println(cmd_input.toInt());
+           motorX.moveTo(y_array[cmd_input.toInt()-1]);
+           digitalWrite(EN, LOW);
+        }
+        Serial.println ("Invalid position");
+      }
        
     }
     Serial.println ("Invalid command");
   } //end of command treatment
   motorX.run();
- // if (motorX.run()){
- //   digitalWrite(EN, LOW);
- // }
- // else {
- //   digitalWrite(EN, HIGH);
- // }
-    /*{
-      if (numero == '1')
-      {
-        Serial.println("Numero 1 recebido - Girando motor sentido horario.");
-        digitalWrite(EN, LOW);
-        sentido_horario = 1;
-        sentido_antihorario = 0;
-      }
 
-      if (numero == '2')
-      {
-        Serial.println("Numero 2 recebido - Girando motor sentido anti-horario.");
-        digitalWrite(EN, LOW);
-        sentido_horario = 0;
-        sentido_antihorario = 1;
-      }
-
-      if (numero == '3')
-      {
-        Serial.println("Numero 3 recebido - Parando motor...");
-        sentido_horario = 0;
-        sentido_antihorario = 0;
-        motorX.moveTo(0);
-        // digitalWrite(EN, HIGH);
-      }
-    }*/
-  /*
-
-  // Move o motor no sentido horario
-  if (sentido_horario == 1)
-  {
-    motorX.moveTo(10000);
-  }
-  // Move o motor no sentido anti-horario
-  if (sentido_antihorario == 1)
-  {
-    motorX.moveTo(-10000);
-  }
-  // Comando para acionar o motor no sentido especificado
-  
-  */
 }
 void calibration() {
   Serial.println("Calibrating...");
@@ -125,12 +98,31 @@ void calibration() {
   max_x_step = motorX.currentPosition();
   Serial.print("Maximum range reached: ");
   Serial.println(max_x_step);
+  
+  while (digitalRead(MIN_Y_PIN) == LOW) {
+    motorY.move(-10000);
+    motorY.run();
+  }
+  motorY.setCurrentPosition(0);
+  Serial.println("Minimum range reached");
+  while (digitalRead(MAX_Y_PIN) == LOW) {
+    motorY.move(1000);
+    motorY.run();
+  }
+  max_y_step = motorY.currentPosition();
+  Serial.print("Maximum range reached: ");
+  Serial.println(max_y_step);
 
   digitalWrite(EN, HIGH);
   x_array[9] = max_x_step/2 ;
+  y_array[8] = max_y_step/2 ;
   x_array[0]= x_array[9] - 9 * STATES_DIF ;
+  y_array[0]= y_array[8] - 9 * STATES_DIF ;
   for (int i=1; i<19;i++){
-    x_array[i]=x_array[i-1];
+    x_array[i]=x_array[i-1]+STATES_DIF;
+  }
+  for (int i=1; i<17;i++){
+    y_array[i]=y_array[i-1]+STATES_DIF;
   }
   
   return;
