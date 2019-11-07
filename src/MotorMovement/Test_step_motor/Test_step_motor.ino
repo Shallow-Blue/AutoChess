31fe5ded722e66dc9faffb840eb08559d6333645
Osmar <osmar.ff.1996@gmail.com>
2019-11-07 10:48:01 +0100

    Change the commands inputs

--- src/MotorMovement/Test_step_motor/Test_step_motor.ino
+++ src/MotorMovement/Test_step_motor/Test_step_motor.ino
@@ -4,7 +4,8 @@
 
 
 // PINOUT
-#define EN 10
+#define EN_X 10
+#define EN_Y 11
 #define STEP 7
 #define DIR 4
 #define MIN_X_PIN 9
@@ -18,15 +19,14 @@
 #define MOTOR_SPEED 250
 #define MOTOR_ACC 100
 
-
-int velocidade_motor = 1000;
-int aceleracao_motor = 100;
 int max_x_step = 0;
 int max_y_step = 0;
 long x_array[19] = {0};
 long y_array [17] = {0};
 String cmd_input;
 int chosen_position = 0;
+int X1,X2,Y1,Y2;
+
 
 
 AccelStepper motorX(1, 7, 4 );
@@ -35,7 +35,8 @@ AccelStepper motorY(2, 8, 5 );
 void setup()
 {
   Serial.begin(9600);
-  pinMode(EN, OUTPUT);
+  pinMode(EN_X, OUTPUT);
+  pinMode(EN_Y, OUTPUT);
   pinMode(MIN_X_PIN, INPUT);
   pinMode(MAX_X_PIN, INPUT);
   pinMode(MAGNET_PIN, OUTPUT);
@@ -47,7 +48,16 @@ void setup()
   motorY.setSpeed(MOTOR_SPEED);
   motorY.setAcceleration(MOTOR_ACC);
   calibration();
-  Serial.println("Type \"AT+GOTO(a number between 1 and 19)(X or Y)\"");
+  /* ***************************
+   * ********COMMANDS***********
+   * ***************************
+   * ||AT+OK                 |  Check communication (Will reply OK)
+   * ||AT+GOTO(X1,Y1,X1,Y2)  |  Bring a piece from the position (X1,Y1) to (X2,Y2) where, either X1 and X2 are equal or Y1 and Y2 are. 
+   *                            Where X1,X2 are numbers between 1 and 19 and Y1 and Y2 are between 1 and 17.
+   * ||AT+CALIB              |  Calibrate the board
+   * 
+   */
+  Serial.println("Type a command");
   
 }
 
@@ -56,31 +66,71 @@ void loop()
   if (Serial.available() > 0)
   {
     cmd_input = Serial.readString();
-    if (cmd_input.startsWith("AT+GOTO")){
-      cmd_input.remove (0,7);
-      if (cmd_input.endsWith("X")){
-        if (cmd_input.toInt()>0 and cmd_input.toInt()<20){
-           Serial.print("Going to position X: ");
-           Serial.println(cmd_input.toInt());
-           motorX.moveTo(x_array[cmd_input.toInt()-1]);
-           digitalWrite(EN, LOW);
+    if (cmd_input.startsWith("AT+")){
+      cmd_input.remove (0,3);
+      if (cmd_input.startsWith("GOTO")){
+        cmd_input.remove (0,5);
+        X1 = cmd_input.toInt();
+        if (X1>9){
+          cmd_input.remove (0,2);  
         }
-        Serial.println ("Invalid position");
-      }
-      if (cmd_input.endsWith("Y")){
-        if (cmd_input.toInt()>0 and cmd_input.toInt()<18){
-           Serial.print("Going to position Y: ");
-           Serial.println(cmd_input.toInt());
-           motorX.moveTo(y_array[cmd_input.toInt()-1]);
-           digitalWrite(EN, LOW);
+        else {
+          cmd_input.remove (0,1);
         }
-        Serial.println ("Invalid position");
+        Y1 = cmd_input.toInt();
+        if (Y1>9){
+          cmd_input.remove (0,2);  
+        }
+        else {
+          cmd_input.remove (0,1);
+        }
+        X2 = cmd_input.toInt();
+        if (X2>9){
+          cmd_input.remove (0,2);  
+        }
+        else {
+          cmd_input.remove (0,1);
+        }
+        Y2 = cmd_input.toInt();
+        
+        digitalWrite(EN_X, LOW);
+        digitalWrite(EN_Y, LOW);
+        while (motorX.run())
+        while (motorY.run())
+        
+        digitalWrite(MAGNET_PIN,HIGH);
+        motorX.moveTo(x_array[X2]);
+        motorY.moveTo(y_array[Y2]);
+        while (motorX.run())
+        while (motorY.run())
+        digitalWrite(MAGNET_PIN,LOW);
+        digitalWrite(EN_X, HIGH);
+        digitalWrite(EN_Y, HIGH);
+  
       }
-       
+      if (cmd_input.startsWith("OK")){
+        Serial.println("OK");
+      }
+      if (cmd_input.startsWith("CALIB")){
+        calibration();
+      }
     }
-    Serial.println ("Invalid command");
+    else{
+      Serial.println ("Invalid command");
+    }
   } //end of command treatment
-  motorX.run();
+  if (motorX.run()){
+    digitalWrite(EN_X, LOW);
+  }
+  else{
+    digitalWrite(EN_X, HIGH);
+  }
+  if (motorY.run()){
+    digitalWrite(EN_Y, LOW);
+  }
+  else{
+    digitalWrite(EN_Y, HIGH);
+  }
 
 }
 void calibration() {
@@ -113,7 +163,8 @@ void calibration() {
   Serial.print("Maximum range reached: ");
   Serial.println(max_y_step);
 
-  digitalWrite(EN, HIGH);
+  digitalWrite(EN_Y, HIGH);
+  digitalWrite(EN_X, HIGH);
   x_array[9] = max_x_step/2 ;
   y_array[8] = max_y_step/2 ;
   x_array[0]= x_array[9] - 9 * STATES_DIF ;
